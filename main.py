e65401e05f5793311f2af4981f2485ec075a7abb
hayde <hayde@LAPTOP-52LE53OM>
2024-01-26 18:00:26 +0000

    updated source

---
+++ main.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+#Find the current directory and read off all files
+current_directory = os.getcwd()
+entries = os.listdir(current_directory)
+files = [f for f in entries if os.path.isfile(os.path.join(current_directory, f))]
+print(files)
+
+files_sanitised = []
+files_csv = []
+
+#Find all .csv files and append to list. Also strip the A.csv from each.
+for file in files:
+    if '.csv' in file:
+        files_csv.append(file)
+        file_stripped = ''.join(letter for letter in file if letter.isdigit())
+        files_sanitised.append(file_stripped)
+    
+    
+voltages = []
+
+#Add the numerical value from the sanitised list to the voltages array
+for i in files_sanitised:
+    voltages.append(float(i))
+
+voltages.sort()
+
+mean_counts = []
+mean_counts_upper = []
+mean_counts_lower = []
+count_err = []
+
+def mean(x):
+    N = len(x)
+    total = 0
+
+    for i in x:
+        total += i
+
+    mean1 = total / N
+
+    return mean1
+
+def variance(x):
+    N = len(x)
+    total_x = 0
+    total_xsquare = 0
+
+    for i in x:
+        total_x += i
+        total_xsquare += i**2
+
+    x_bar = total_x / N
+    x_bar_square = total_xsquare / N
+
+    var = x_bar_square - x_bar**2
+
+    return abs(var)
+
+for file in files_csv:
+    df = pd.read_csv(file)
+    time = df[' time ']
+    count = df[' count0 ']
+
+    intervals = len(time) - 1
+    
+    dt = []
+    dcount = []
+    freq = []
+
+    for i in range (0, intervals):
+        dt_i = time[i+1] - time[i]
+        dt.append(dt_i)
+    
+    for j in range(0,intervals):
+        count_i = count[i+1] - count[i]
+        dcount.append(count_i)
+
+    for k in range(0, intervals):
+        freq_i = dcount[k] / dt[k]
+        freq_i *= 1000 #Convert from per millisecond to second
+        freq.append(freq_i)
+
+    count_avg = mean(freq)
+    mean_counts.append(count_avg)
+
+
+log_counts = np.log(mean_counts)
+plt.plot(voltages, log_counts)
+plt.xlabel('Voltages/mV')
+plt.ylabel('log(Frequency / Hz)')
+plt.title('Dark Count Rate vs Threshold Voltage')
+plt.savefig('Dark Count 1')
+plt.show()
+
---
+++ readme.txt
@@ -0,0 +1,3 @@
+NOTE:
+Check the whitespaces in the CSV file. It looked like the column headers have a space before and after.
+Should work by stripping any non numbers from the CSV file name. Just make sure each file has the voltage number somewhere inside AND that the voltages are the ONLY numbers.
